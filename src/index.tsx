dfc6e39b7742c93f2df0a79ee74007d05e394f30
brunnobird <bird.tec.gonzalez@gmail.com>
2022-06-14 22:34:17 -0300

    feat: mock backend with mirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    //Diz que todas as chamadas para api estão apartir do "/api" da URL da rota
+    this.namespace = 'api';
+    //delay
+    this.timing = 2000;
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Desenvolvimento de website',
+          amount: 14.000,
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          createdAt: new Date(),
+        }
+      ]
+    });
+  }
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
